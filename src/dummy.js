7bdd8735162948554378b9f22296e4e7d7a2a10a
mininerej <ejhong.4991@gmail.com>
2019-03-10 13:50:01 +0900

    initialize data

--- src/dummy.js
+++ src/dummy.js
@@ -1,53 +1,53 @@
 export const fakeList = [
-    {
-        category: "travel",
-        title: "exchange money",
-        done: true,
-        datetime: null,
-        memo: "won to yen"
-    },
-    {
-        category: "travel",
-        title: "search good restaurant",
-        done: false,
-        datetime: null,
-        memo: "mukbang good~"
-    },
-    {
-        category: "travel",
-        title: "prepare guide book",
-        done: false,
-        datetime: null,
-        memo: "tokyo guide book"
-    },
-    {
-        category: "travel",
-        title: "get ticket to Narita",
-        done: true,
-        datetime: null,
-        memo: ""
-    },
-    {
-        category: "school",
-        title: "math class homework",
-        done: true,
-        datetime: null,
-        memo: ""
-    },
-    {
-        category: "school",
-        title: "prepare for tomorrow",
-        done: false,
-        datetime: null,
-        memo: ""
-    },
-    {
-        category: "codestates",
-        title: "coding",
-        done: true,
-        datetime: null,
-        memo: ""
-    },
+    // {
+    //     category: "travel",
+    //     title: "exchange money",
+    //     done: true,
+    //     datetime: null,
+    //     memo: "won to yen"
+    // },
+    // {
+    //     category: "travel",
+    //     title: "search good restaurant",
+    //     done: false,
+    //     datetime: null,
+    //     memo: "mukbang good~"
+    // },
+    // {
+    //     category: "travel",
+    //     title: "prepare guide book",
+    //     done: false,
+    //     datetime: null,
+    //     memo: "tokyo guide book"
+    // },
+    // {
+    //     category: "travel",
+    //     title: "get ticket to Narita",
+    //     done: true,
+    //     datetime: null,
+    //     memo: ""
+    // },
+    // {
+    //     category: "school",
+    //     title: "math class homework",
+    //     done: true,
+    //     datetime: null,
+    //     memo: ""
+    // },
+    // {
+    //     category: "school",
+    //     title: "prepare for tomorrow",
+    //     done: false,
+    //     datetime: null,
+    //     memo: ""
+    // },
+    // {
+    //     category: "codestates",
+    //     title: "coding",
+    //     done: true,
+    //     datetime: null,
+    //     memo: ""
+    // },
 ];
 
-export const categories = ['travel', 'school', 'codestates']
+export const categories = ['todo list']
